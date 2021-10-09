292c6990a8777cdd1c90f039be18baffe3809cca
ramiboutas <ramiboutas@hotmail.com>
2021-10-09 14:34:04 +0200

    slight changes on html

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('xslx/', include('xlsxproducts.urls') ),
+    path('xlsx/', include('xlsxproducts.urls') ),
 ]
