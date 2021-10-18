658381607fab523fd460a9eeaa1f0c9aadb74b5b
ramiboutas <ramiboutas@hotmail.com>
2021-10-18 12:42:00 +0200

    AbstractProduct

--- products/models.py
+++ products/models.py
@@ -2,16 +2,22 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 
-class Product(models.Model):
+class AbstractProduct(models.Model):
     title = models.CharField(max_length=255, unique=True)
     price = models.IntegerField(default=0)
     type =  models.CharField(max_length=20)
     description = models.TextField()
     slug = models.SlugField(null=True, blank=True)
 
+    class Meta:
+        abstract = True
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Product, self).save(*args, **kwargs)
+
+class XlsxProduct(AbstractProduct):
+    year_versions = models.CharField(max_length=255) # implement: multiple choices!
 
     def __str__(self):
         return f'{self.title}'
