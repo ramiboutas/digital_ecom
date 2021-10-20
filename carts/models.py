6c0afacfc2c3b11e5a4738aeba74711b43701e81
ramiboutas <ramiboutas@hotmail.com>
2021-10-20 16:26:23 +0200

    CartItem model

--- carts/models.py
+++ carts/models.py
@@ -1,13 +1,34 @@
 from django.db import models
-
+from products.models import Product
 
 class CartAbstract(models.Model):
     pass
 
 
 class CartItem(CartAbstract):
-    pass
+    cart_id = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    # quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, unique=False)
 
+    class Meta:
+        ordering = ['date_added']
+
+    def total(self):
+        return self.product.price
+
+    def name(self):
+        return self.product.name
+
+    def price(self):
+        return self.product.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+
+    def augment_quantity(self, quantity):
+        self.quantity = self.quantity + int(quantity)
+        self.save()
 
 class Cart(CartAbstract):
     pass
